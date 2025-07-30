8c049bfabef7288b6fc0dce879efe6d7ab72e9e0
Alaa-Atwa <alaa.atwa555@gmail.com>
2025-07-30 20:22:32 +0300

    first commit

---
+++ dir_cleaner.py
@@ -0,0 +1,41 @@
+import os 
+import shutil 
+
+# file cleaner for current directory
+
+print("Sort and Clean Directories")
+print("==============================")
+target_dir = input("Press Enter for current directory, or enter a different path:  ")
+if target_dir == "":
+  target_dir = os.getcwd()
+
+os.chdir(target_dir)
+
+files = [file for file in os.listdir() if os.path.isfile(file)] # list files and ignore dirs.
+extensions = set()  # a set to collect unique extensions
+
+# extract extension
+for file in files:
+  if "." in file and not file.startswith("."):
+    ext = file.rsplit('.', 1)[1]
+    extensions.add(ext)
+
+
+for ext in extensions:
+  os.makedirs(ext, exist_ok=True)
+
+for file in files:
+  if "." in file and not file.startswith("."):
+    ext = file.rsplit('.', 1)[1]
+    if ext in file:
+      shutil.move(file, ext)
+      print(f"{file} moved to {ext} directory.")
+
+print("===============================================")
+print("Done Sorting and Cleannnig Files.")
+print("===============================================")
+  
+
+
+
+
